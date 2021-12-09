89e5ea58e99e199156e95c3b50822a7e282436fd
psibuck <psibuck@gmail.com>
2021-12-09 09:40:53 +0000

    Hydrothermal Challenge Complete

--- hydrothermal.py
+++ hydrothermal.py
@@ -2,51 +2,33 @@ import os
 
 def ReadData():
     vectors = []
-    with open(os.getcwd() + "/data/hydrothermal_test.txt") as file:
+    with open(os.getcwd() + "/data/hydrothermal.txt") as file:
         for line in file:
             entry = line.strip("\n")
             entry = entry.split(" -> ")
             vectors.append(entry)
-    print(vectors)
     return vectors
 
 class Line:
     def __init__(self):
-        self.min = -1
-        self.max = -1
+        self.values = []
 
     def SingleCoord(self, coord):
-        if coord <= self.max and coord >= self.min:
+        while coord >= len(self.values):
+            self.values.append(0)
+
+        self.values[coord] += 1
+        if self.values[coord] == 2:
             return 1
-        elif coord < self.max:
-            self.max = coord
-
-
-        self.max = coord
-        self.min = coord
-
         return 0
 
     def Process(self, coord1, coord2):
-        maxval = max(coord1, coord2)
-        minval = min(coord1, coord2)
-        overlaps = 0
-
-        if self.max == -1:
-            self.max = maxval
-            self.min = minval
-            return overlaps
-
-        lower_bound = max(self.min, minval)
-        upper_bound = min(self.max, maxval)
-        overlaps = max(0, upper_bound - lower_bound + 1)
-
-        if self.max < max:
-            self.max = maxval
-        if self.min > min:
-            self.min = minval
-
-        return overlaps
+        i = min(coord1, coord2)
+        count = 0
+        while i <= max(coord1, coord2):
+            count += self.SingleCoord(i)
+            i += 1
+        return count
 
 class Application:
 
@@ -55,23 +37,20 @@ class Application:
         self.count = []
 
     def ProcessXCoordinate(self, y, x1, x2):
-        xmin = min(x1, x2)
-        xmax = max(x1, x2)
-
-        while len(self.count) < xmax:
+        while len(self.count) <= max(x1,x2):
             self.count.append(0)
         
-        i = xmin
-        while i <= xmax:
+        i = min(x1,x2)
+        while i <= max(x1,x2):
             entry = self.count[i]
             if entry == 0:
                 entry = Line()
                 self.count[i] = entry
-            self.overlaps += entry.SingleCoord(i)
+            self.overlaps += entry.SingleCoord(y)
             i += 1
 
     def ProcessYCoordinate(self, x, y1, y2):
-        while x > len(self.count):
+        while x >= len(self.count):
             self.count.append(0)
         
         entry = self.count[x]
@@ -80,22 +59,61 @@ class Application:
             self.count[x] = entry
         
         self.overlaps += entry.Process(y1, y2)
-
-    def SimpleThermos(self):
-        data = ReadData()
-        for entry in data:
-            vector = entry.split(",")
-            vec1 = vector[0]
-            vec2 = vector[1]
-
-            x1 = vec1[0]
-            x2 = vec2[0]
-            y1 = vec1[1]
-            y2 = vec2[1]
-
-            if x1 == x2:
-                ProcessYCoordinates(x1, y1, y2)
-            else:
-                ProcessXCoordinates(y1, x1, x2)
+    
+    def ProcessDiagCoordinate(self, x1,x2,y1,y2):
+        xcrement = 1
+        ycrement = 1
+        if x2 < x1:
+            xcrement = -1
+        if y2 < y1:
+            ycrement = -1
+    
+        while True:
+            self.ProcessXCoordinate(y1, x1, x1)
 
 
+            if x1 == x2 and y1 == y2:
+                break
+            x1 += xcrement
+            y1 += ycrement
+
+
+def SimpleThermos():
+    app = Application()
+    data = ReadData()
+    for entry in data:
+        # print("LOOP")
+        # for value in app.count:
+        #     if value == 0:
+        #         print(0)
+        #     else:
+        #         print(value.values)
+        vec1 = entry[0]
+        vec2 = entry[1]
+        
+        initcoords = vec1.split(",")
+        endcoords = vec2.split(",")
+        x1 = int(initcoords[0])
+        x2 = int(endcoords[0])
+        y1 = int(initcoords[1])
+        y2 = int(endcoords[1])
+
+        if x1 == x2:
+            app.ProcessYCoordinate(x1, y1, y2)
+        elif y1 == y2:
+            app.ProcessXCoordinate(y1, x1, x2)
+        else:
+            app.ProcessDiagCoordinate(x1,x2,y1,y2)
+
+    print(app.overlaps)
+    # overlaps = 0
+    # for data in app.count:
+    #     if data == 0:
+    #         continue
+    #     for count in data.values:
+    #         if count >= 2:
+    #             overlaps += 1
+
+  #  print(overlaps)
+
+SimpleThermos()
