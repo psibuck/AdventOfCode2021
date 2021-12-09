dd2ba161e03df55b2df731f257ce82e5839db766
psibuck <psibuck@gmail.com>
2021-12-09 19:32:46 +0000

    fix error in file generator

--- tools/generate_files.py
+++ tools/generate_files.py
@@ -6,7 +6,7 @@ filename = input("Please type the name of the files you want to create \n")
 with open(os.getcwd() + "/" + filename + ".py", "w") as file:
     file.write("import os \n\n")
     file.write("def ReadData():\n")
-    file.write("\twith open(os.getcwd() + /data/" + filename + ".txt) as file:\n") 
+    file.write("\twith open(os.getcwd() + '/data/" + filename + "'.txt) as file:\n") 
     file.write("\t\tfor line in file:")
     pass
 
