55255dbd918234f280b22b26e937dca0ebb7710a
psibuck <psibuck@gmail.com>
2021-12-09 17:51:24 +0000

    Update the generate file script to save a bit more time

--- tools/generate_files.py
+++ tools/generate_files.py
@@ -4,6 +4,10 @@ filename = input("Please type the name of the files you want to create \n")
 
 
 with open(os.getcwd() + "/" + filename + ".py", "w") as file:
+    file.write("import os \n\n")
+    file.write("def ReadData():\n")
+    file.write("\twith open(os.getcwd() + /data/" + filename + ".txt) as file:\n") 
+    file.write("\t\tfor line in file:")
     pass
 
 data_filepath = os.getcwd() + "/data/"
